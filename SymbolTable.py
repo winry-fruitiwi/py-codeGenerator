e783231c6fe31633c346358279dd2c4d50d85f83
Winry <winry.tian@gmail.com>
2022-11-22 15:01:35 -0800

    add symbolTable.typeOf, fix argument order in test

--- SymbolTable.py
+++ SymbolTable.py
@@ -11,7 +11,7 @@ class SymbolTable:
 
     # defines new identifier of a given name, type, kind, and index (varCount).
     def define(self, name, type, kind):
-        if kind == "STATIC" or "FIELD":
+        if kind == "STATIC" or kind == "FIELD":
             self.classTable[name] = [type, kind, self.varCount(kind)]
         else:
             self.subroutineTable[name] = [type, kind, self.varCount(kind)]
@@ -20,7 +20,7 @@ class SymbolTable:
     def varCount(self, kind):
         kindCount = 0
 
-        if kind == "STATIC" or "FIELD":
+        if kind == "STATIC" or kind == "FIELD":
             target = self.classTable
         else:
             target = self.subroutineTable
@@ -34,17 +34,18 @@ class SymbolTable:
     # a helper function that says which table name is in.
     def findNameInTables(self, name):
         if name in self.subroutineTable:
-            return self.subroutineTable
-        return self.classTable
+            return self.subroutineTable[name]
+        return self.classTable[name]
 
     # finds the kind of the given identifier
     def kindOf(self, name):
         table = self.findNameInTables(name)
-        return table[name][1]
+        return table[1]
 
     # finds the type of the given identifier
     def typeOf(self, name):
-        pass
+        table = self.findNameInTables(name)
+        return table[0]
 
     # finds the index of the given identifier
     def indexOf(self, name):
